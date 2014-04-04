c00d01e580699cff77ceb46efbb3cb0cdf1ca75d
KevinKrames <kevinkrames@gmail.com>
2014-04-04 12:43:31 -0400

    Added a basic main function that will error check and take input from the user. Doesn't do anything yet.

--- Project1/src/Project1.cpp
+++ Project1/src/Project1.cpp
@@ -7,9 +7,54 @@
 //============================================================================
 
 #include <iostream>
+#include <exception>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+	bool mainloop = true;
+
+	string inputString = "";
+
+	while (mainloop) {
+		//Short output string describing the program
+		cout << "This program will be a fricken awesome calculator."
+				<< endl;
+		//Grab the input for value1 using a loop to error check
+		bool loop1 = true;
+		while (loop1) {
+			try {
+				//Output to console to tell the user to input
+				cout
+						<< "Input a command:";
+				if (cin >> inputString) {
+					if (inputString == "+") {
+						loop1 = false;
+						cout << endl;
+
+						cin.clear();
+						cin.ignore();
+					} else {
+						//Failed to receive input
+						cin.clear();
+						cin.ignore();
+						throw 1;
+					}
+
+				} else {
+					//Failed to receive input
+					throw 1;
+					cin.clear();
+					cin.ignore();
+				} // end cin >> inputString if statement
+
+			} catch (int e) { // catch the errors
+				if (e == 1) {
+					cout << "Invalid input, try again." << endl;
+				}
+			}
+		} // End loop1 while statement
+		return 0;
+	}
 }
